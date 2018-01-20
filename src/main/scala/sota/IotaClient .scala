ae2f9fa8e5ebcc1a83648f97a2db725e3d78e096
ahab <abdshah94@gmail.com>
2018-01-20 17:38:36 +0500

    add api suite

--- src/main/scala/sota/IotaClient .scala
+++ src/main/scala/sota/IotaClient .scala
@@ -7,8 +7,8 @@ import com.softwaremill.sttp._
   *
   * @author ahab94
   */
-case class IotaClientConfig(protocol: String = "http", host: String = "localhost", port: String = "14265") {
-  val iriUrl: String = protocol + "://" + host + ":" + port
+case class IotaClientConfig(protocol: String = "http", host: String = "localhost", port: Int = 14265) {
+  val iriUrl: Uri = Uri(protocol, host, port)
 }
 
 class IotaClient(config: IotaClientConfig = IotaClientConfig(),
