0fb60c58234650b677f3b247f04eb0f3da66068d
ahab <abdshah94@gmail.com>
2018-01-21 17:35:08 +0500

    add WIP tag

--- src/main/scala/sota/IotaClient.scala
+++ src/main/scala/sota/IotaClient.scala
@@ -1,6 +1,8 @@
 package sota
 
 import com.softwaremill.sttp._
+import jota.IotaAPI
+import jota.IotaAPI._
 import sota.apis.{IotaAPICore, IotaClientConfig}
 
 /**
@@ -14,4 +16,10 @@ class IotaClient(config: IotaClientConfig = IotaClientConfig(),
                  implicit val apiBackend: SttpBackend[Id, Nothing] = HttpURLConnectionBackend())
   extends IotaAPICore(config) {
   //WIP
+}
+
+object test extends App {
+  val x = new IotaAPI
+
+x.addRemainder()
 }
