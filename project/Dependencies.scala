68a5a746e74c2ad42483e9b163e9c4a3165a34df
ahab <abdshah94@gmail.com>
2018-01-30 19:32:11 +0500

    add logging dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,10 +8,15 @@ object Dependencies {
 
   val json = Seq("com.softwaremill.sttp" %% "json4s" % "1.1.4")
 
+  val utils = Seq( "com.typesafe.scala-logging" %% "scala-logging" % "3.7.2",
+    "ch.qos.logback" % "logback-classic" % "1.2.3" % Runtime
+  )
+
   val all: Seq[ModuleID] =
     http ++
       json ++
-      iotaLibs
+      iotaLibs ++
+      utils
 
   val resolvers = Seq(
     "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/",
